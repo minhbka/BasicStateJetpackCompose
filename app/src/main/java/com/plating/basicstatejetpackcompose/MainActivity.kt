2c9832019cc27e6bcc8dbb6ff3d31929d089213d
minhbka <minhbka@gmail.com>
2022-11-04 16:42:00 +0900

    state driven ui

--- app/src/main/java/com/plating/basicstatejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/plating/basicstatejetpackcompose/MainActivity.kt
@@ -10,7 +10,8 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,12 +36,14 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun WaterCounter(modifier: Modifier=Modifier){
         Column(modifier = modifier.padding(16.dp)) {
-            var count = 0
-            Text(
-                text = "you've had $count glasses.",
-                modifier = modifier.padding(16.dp)
-            )
-            Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            var count by rememberSaveable  { mutableStateOf(0)}
+
+            if (count > 0) {
+                Text(
+                    text = "you've had $count glasses.",
+                )
+            }
+            Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count<10) {
                 Text("Add one")
             }
         }
